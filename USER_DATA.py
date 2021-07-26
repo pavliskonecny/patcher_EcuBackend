a58719a5833316f3af47908354e97e5abbee74f2
Pavel Konecny <pavel.konecny@hella.com>
2021-07-26 09:25:26 +0200

    change absolute path to relative path

--- USER_DATA.py
+++ USER_DATA.py
@@ -2,7 +2,7 @@
 Specify patch "project & number & description" appears on main window and use as executable file name.
 """
 #!!! patch number must be unique because of creating unique backup file names!!!
-PATCH_NUMBER = "patch1"
+PATCH_NUMBER = "patch2"
 PATCH_PROJECT = "Audi Q6"
 PATCH_DESCRIPTION = "added direct fan control + signal animations"
 
@@ -10,8 +10,6 @@ PATCH_DESCRIPTION = "added direct fan control + signal animations"
 Specify the relative path for files what should be stored to executable file and replaced later.
 """
 PATCH_FILES = [
-    #"z_test_patcher/one.jpg",
-    #"z_test_patcher/two.jpg",
     "VAG/AudiQ6/AudiQ6Headlamp.py",
     "VAG/AudiQ6/AudiQ6MXBCCCService.py"
 ]
