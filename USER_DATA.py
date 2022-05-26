111db2401699a4050089a3b0d7c6b679abe94671
Pavel Konecny <pavel.konecny@hella.com>
2022-05-26 11:15:18 +0200

    changed user data only

--- USER_DATA.py
+++ USER_DATA.py
@@ -1,15 +1,15 @@
 """
 Specify patch "project & number & description" appears on main window and use as executable file name.
+!!! patch number must be unique because of creating unique backup file names!!!
 """
-#!!! patch number must be unique because of creating unique backup file names!!!
-PATCH_NUMBER = "patch2"
-PATCH_PROJECT = "Audi Q6"
-PATCH_DESCRIPTION = "added direct fan control + signal animations"
+PATCH_NUMBER = "patch1"
+PATCH_PROJECT = "VW 416 PA2"
+PATCH_DESCRIPTION = f"Fixed DCL reference issue for right side headlamps"
 
 """
 Specify the relative path for files what should be stored to executable file and replaced later.
 """
 PATCH_FILES = [
-    "VAG/AudiQ6/AudiQ6Headlamp.py",
-    "VAG/AudiQ6/AudiQ6MXBCCCService.py"
+    "ecuserver/VAG/VW416PA2/VW416PA2BaseService.py",
+    "ecuserver/Ecu/LLPMax/LLP_max.py",
 ]
