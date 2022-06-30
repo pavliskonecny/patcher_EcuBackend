4be44683b775545977df557fa553ad7da44b86d5
Pavel Konecny <pavel.konecny@hella.com>
2022-06-30 10:15:57 +0200

    changed user data only - Lambo

--- USER_DATA.py
+++ USER_DATA.py
@@ -3,13 +3,16 @@ Specify patch "project & number & description" appears on main window and use as
 !!! patch number must be unique because of creating unique backup file names!!!
 """
 PATCH_NUMBER = "patch1"
-PATCH_PROJECT = "VW 416 GP"
-PATCH_DESCRIPTION = f"USE THIS PATCH FOR ECUBACKEND 22.05.17 ONLY !!!\n\n" \
-                    f"- Fixed DCL reference issue\n"
+PATCH_PROJECT = "Lamborghini LB744"
+PATCH_DESCRIPTION = f"USE THIS PATCH FOR EcuBackend 22.04.28 ONLY !!!\n\n" \
+                    f"- added control of addHB via light distributions\n\n" \
+                    f"- added reading of VW FAZIT String"
 
 """
 Specify the relative path for files what should be stored to executable file and replaced later.
 """
 PATCH_FILES = [
-    "ecuserver/VAG/vagservice.py",
+    "ecuserver/VAG/LamborghiniLB744/LamborghiniLB744.py",
+    "ecuserver/VAG/LamborghiniLB744/LamborghiniLB744Service.py",
+    "ecuserver/VAG/LamborghiniLB744/mxb_distribution_definition.py",
 ]
