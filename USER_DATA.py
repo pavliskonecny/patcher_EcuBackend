dc36988ce387c78e72b7212c59135da4c6386086
Pavel Konecny <pavel.konecny@hella.com>
2024-01-26 09:52:54 +0100

    just TEST - delete it

--- USER_DATA.py
+++ USER_DATA.py
@@ -2,25 +2,21 @@
 Specify patch "project & number & description" appears on main window and use as executable file name.
 !!! patch number must be unique because of creating unique backup file names!!!
 """
-PATCH_NUMBER = "patch1"
-PATCH_PROJECT = "LiXiang X01"
-PATCH_DESCRIPTION = f"USE THIS PATCH FOR EcuBackend 22.08.23 ONLY !!!\n\n" \
-                    f"- added services FanControl, MotorLeveler, SecurityAccess"
+PATCH_NUMBER = "patchX"
+PATCH_PROJECT = "AudiQ6"
+PATCH_DESCRIPTION = f"USE THIS PATCH FOR EcuBackend 22.XX.XX ONLY !!!\n\n" \
+                    f"test iTACu"
 
 """
 Specify the relative path for files what should be stored to executable file and replaced later.
 """
 PATCH_FILES = [
-    "ecuserver/Ecu/LHCM/LHCM.py",
-    "ecuserver/Ecu/LHCM/security_handler.py",
-    "ecuserver/EcuServicesFuncs/LHCMServiceFunctions.py",
-    "ecuserver/LiXiang/LiaX01/LiaX01Service.py",
-    "ecuserver/CAN_Trace/ECU/LHCM_CanCyclic.csv"
+    "ecuserver/VAG/vagservice.py"
 ]
 
 """
 Specify the relative path for files what are new and it is not possible to backup. But they must be in PATCH_FILES !
 """
 NEW_PATCH_FILES = [
-    "ecuserver/Ecu/LHCM/security_handler.py",
 ]
+"Test"
